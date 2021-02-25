bd597dd8e1816ac0a159e18e66725708e13db6c3
Primoz Hrovat <primoz.hrovat@bearingpoint.com>
2021-02-25 14:02:56 +0100

    fixed tests

--- src/test/kotlin/si/hrovat/micronaut/HelloClientSpec.kt
+++ src/test/kotlin/si/hrovat/micronaut/HelloClientSpec.kt
@@ -1,9 +1,21 @@
 package si.hrovat.micronaut
 
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.client.annotation.Client
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import io.reactivex.Single
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import javax.inject.Inject
+
+@Client("/application-info")
+interface HelloClient {
+
+    @Get(consumes = [MediaType.TEXT_PLAIN])
+    fun applicationInfo(): Single<String>
+}
+
 
 @MicronautTest
 class HelloClientSpec {
